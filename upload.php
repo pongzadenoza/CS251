907ec3f7993b80fc8f166237dbfe30f6acbf9fa8
pongzadenoza@gmail.com <32919498+pongzadenoza@users.noreply.github.com>
2018-04-19 08:05:51 +0700

    add item into DB

--- upload.php
+++ upload.php
@@ -1,49 +1,66 @@
 <?php
 $target_dir = "img/product/";
-print_r($_REQUEST);
-print_r($_FILES);
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+print_r( $_REQUEST );
+print_r( $_FILES );
+$target_file = $target_dir . basename( $_FILES[ "fileToUpload" ][ "name" ] );
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower( pathinfo( $target_file, PATHINFO_EXTENSION ) );
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
+if ( isset( $_POST[ "submit" ] ) ) {
+	$check = getimagesize( $_FILES[ "fileToUpload" ][ "tmp_name" ] );
+	if ( $check !== false ) {
+		echo "File is an image - " . $check[ "mime" ] . ".";
+		$uploadOk = 1;
+	} else {
+		echo "File is not an image.";
+		$uploadOk = 0;
+	}
 }
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
+if ( file_exists( $target_file ) ) {
+	echo "Sorry, file already exists.";
+	$uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
+if ( $_FILES[ "fileToUpload" ][ "size" ] > 500000000 ) {
+	echo "Sorry, your file is too large.";
+	$uploadOk = 0;
 }
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
+if ( $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" &&
+	$imageFileType != "gif" ) {
+	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	$uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+if ( $uploadOk == 0 ) {
+	echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	if ( move_uploaded_file( $_FILES[ "fileToUpload" ][ "tmp_name" ], $target_file ) ) {
+		$name = $_REQUEST[ 'p_name' ];
+		$price = $_REQUEST[ 'p_price' ];
+		$cate = $_REQUEST[ 'p_cate' ];
+		$detail = $_REQUEST[ 'p_dtl' ];
+		$age = $_REQUEST[ 'p_age' ];
+		$fName = $_FILES[ 'fileToUpload' ][ 'name' ];
+
+		$conn = new mysqli( "13.231.233.64:3306", "project", "123456", "CS281" );
 		
-		$conn = new mysqli("13.231.233.64:3306","project","123456","CS281");
-		$sql = "INSERT INTO product(p_id , )";
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
-    }
+		
+		
+		$sql1 = "INSERT INTO product(p_id,p_name,p_price,p_age,p_category,p_detail) VALUES('" . $fName . "','" . $name . "','" . $price . "','".$age."','" . $cate . "','" . $detail . "');";
+		
+		
+		if($conn->query( $sql1 )===TRUE){
+			echo "add to DB sucess";
+		}
+		else{
+			echo $conn -> error;
+		}
+		echo "The file " . basename( $_FILES[ "fileToUpload" ][ "name" ] ) . " has been uploaded.";
+	} else {
+		echo "Sorry, there was an error uploading your file.";
+	}
 }
 ?>
