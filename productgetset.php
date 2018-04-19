3863e3f47b58720087330bef4df69ce025072ce4
porschelook <33505600+porschelook@users.noreply.github.com>
2018-04-19 10:34:39 +0700

    getorder

--- productgetset.php
+++ productgetset.php
@@ -2,6 +2,8 @@
 
 class Product
 {
+		
+	private $Subtotal;
 
     private $pId, $pName, $pPrice, $pInfo, $pCategory, $pAge;
 
@@ -9,43 +11,38 @@ class Product
     {
         $this->pId = $pId;
         $this->pName = $pName;
-        
         $this->pPrice = $pPrice;
         $this->pInfo = $pInfo;
         $this->pCategory = $pCategory;
-        $this->pAge = $pAge;
+        $this->pAge = $pAge;		
+  }public function getSubtotal()
+    {
+        return $this->Subtotal;
     }
 	public function getPAge()
     {
         return $this->pAge;
     }
-
     public function getPId()
     {
         return $this->pId;
     }
-
     public function getPName()
     {
         return $this->pName;
     }
-
     public function getPPrice()
     {
         return $this->pPrice;
     }
-
     public function getPInfo()
     {
         return $this->pInfo;
     }
-
-    public function getPCategory()
+   public function getPCategory()
     {
         return $this->pCategory;
-    }
-	
-	
+    }	
 	 public function setPId($pId)
     {
         $this->pId = $pId;
@@ -54,12 +51,6 @@ class Product
     {
         $this->pName = $pName;
     }
-	
-	
-	
-	
-	
-	
 	 public function setPPrice($pPrice)
     {
         $this->pPrice = $pPrice;
@@ -77,6 +68,61 @@ class Product
     {
         $this->pInfo = $pInfo;
     }
+ public function setSubtotal($Subtotal)
+    {
+        $this->Subtotal = $Subtotal;
+    }
+
+	public function additem($conn)
+    { $sql = "INSERT INTO Shopping Cart
+              (p_id,p_price,SubTotal )
+              VALUES
+              ('".$this->pId."','".$this->pPrice."','".$this->Subtotal."')";
+    $conn->query($sql) ;
+  
+    }
+
+	public function removeitem($conn)
+    {
+        $sql = "INSERT INTO Shopping Cart
+              (p_id,p_price,SubTotal )
+              VALUES
+              ('".$this->pId."','".$this->pPrice."','".$this->Subtotal."')";
+    $conn->query($sql) ;
+    }
+
+	
+
+	public function getOrder($conn) {
+
+      $sql = "SELECT * FROM Shopping Cart ";
+      $rs = $conn->query($sql) ;
+
+      $temp = array();
+
+      while ($data = $rs->fetch_array()) {
+
+          $productQ = new Product($data['p_id'],$data['p_price'],$data['p_SubTotal']);
+          array_push($temp,$productQ);
+      }
+
+      return $temp;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
 
