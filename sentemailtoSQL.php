a40a3154de23cdbe9fe5e25a9f1b1aa08d40b6b4
Korpong <33505027+korpongg@users.noreply.github.com>
2018-05-11 00:53:29 +0700

    Add files via upload

--- sentemailtoSQL.php
+++ sentemailtoSQL.php
@@ -2,10 +2,10 @@
 <?php
 include("emailclass.php");
 session_start();
-
+$usr = $_SESSION['C_ID'];
 	$ti ;
 
-print_r($_REQUEST);
+
 
 $regis = new emailclass();
 
@@ -14,10 +14,21 @@ $regis = new emailclass();
 if($regis->chkConnect()){
 
 
-	$regis->setRegisInfo($_REQUEST['id'],$_REQUEST['name'] ,$_REQUEST['email']  );
+	$regis->setRegisInfo($usr,$_REQUEST['email']  );
 
 	$regis->regis();
 }
 
 
 ?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	<meta http-equiv="refresh" content="0;url=../index.php">
+</body>
+</html>
