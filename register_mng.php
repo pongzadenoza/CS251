da1112590a5dffccd931aa1a19f3c740532e0603
pongzadenoza@gmail.com <32919498+pongzadenoza@users.noreply.github.com>
2018-05-17 03:22:23 +0700

    update font

--- register_mng.php
+++ register_mng.php
@@ -17,6 +17,7 @@ if($regis->chkConnect()){
 	$regis->setRegisInfo($_REQUEST['cname'] ,$_REQUEST['cmail'] ,$_REQUEST['cphone'] ,$_REQUEST['cads'],$_REQUEST['cusr'],$_REQUEST['cpwd'], $_REQUEST['ctype']);
 	
 	$regis->regis();
+	header('location:login.php');
 }
  
 
